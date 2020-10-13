3e6ec071e3c8fa01e5ce6c2bca0d208c0ba6a6d9
Shrey Rai <kumarshrey.21099@gmail.com>
2020-10-13 14:40:37 +0530

    Added feature of writing blogs

--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Shrey Rai',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'October, 2020'
-    },
-    {
-        'author': 'Kumar Shrey',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2020'
-    }
-]
+
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
+
